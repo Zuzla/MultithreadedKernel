cee587804dcab5cbe3df4e47b153a7f0c6b8b08d
Zuzla <ilyagaevsiy@icloud.com>
2023-10-26 18:50:47 +0000

    Added reading from the hard disk in real mode

--- boot.asm
+++ boot.asm
@@ -1,8 +1,39 @@
-ORG 0x7c00
+ORG 0
 BITS 16
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
 
 start:
-    mov si, message
+    jmp 0x7c0:step2
+
+step2:
+    cli                 ; Clear Intrerupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti                 ; Enable Interrupts
+
+    mov ah, 2           ; Read sector command
+    mov al, 1           ; one sector to read
+    mov ch, 0           ; Culinder ow 8 bits
+    mov cl, 2           ; Read sector two
+    mov dh, 0           ; Head number
+    mov bx, buffer
+    int 0x13
+    jc error
+
+    mov si, buffer
+    call print    
+    jmp $
+
+error:
+    mov si, error_message
     call print
     jmp $
 
@@ -23,6 +54,7 @@ print_char:
     ret
 
 message: db 'Hello World!', 0
+error_message: db 'Failed to load sector!', 0
 
 times 510-($ - $$) db 0 ;   $ means current address and $$ means the first address of the current section. 
                         ;   You have to understand that the times directive only operates on numbers and the difference of address ( $-$$ ) yields a number (Offset). 
@@ -34,3 +66,5 @@ times 510-($ - $$) db 0 ;   $ means current address and $$ means the first addre
                         ;   instruction is at the 15th byte. Now 510 - 15 will give you 495 i.e 495 bytes from current instruction will be padded with zeros by times directive.
                         ;   if you dissect the binary for times 510 -( 0x000f - 0x0000 ) db 0 alone, it will exactly have 495 zeros!!
 dw 0xAA55
+
+buffer:
